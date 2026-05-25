24495945be225c50ca092ff11db0a7471e658736
Anne Grethe <anne-grethe.knutsen@atea-no>
2026-05-25 12:56:42 +0200

    Legg til enkel modulkonfig i hovslager

---
+++ js/moduler.js
@@ -0,0 +1,16 @@
+window.AGK_MODULER = {
+  timer: true,
+  kunder: true,
+  ansatte: true,
+  firma: true,
+
+  faktura: true,
+  kreditnota: true,
+  purring: true,
+
+  varer: false,
+  lager: false,
+  lonn: false,
+
+  hovslager: true
+};
